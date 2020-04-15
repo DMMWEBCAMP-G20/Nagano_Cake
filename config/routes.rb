ccff46aeeff2adeea5aeae5e9bc3425bf2dcd905
matsubishi5 <matsubishi5@yahoo.co.jp>
2020-04-15 05:06:15 +0000

    fix_routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,4 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  get 'homes/about'
-  namespace :admin do
-    get 'orders/index'
-    get 'orders/show'
-    get 'orders/edit'
-    get 'orders/update'
-  end
-  namespace :admin do
-    get 'order_products/show'
-    get 'order_products/edit'
-    get 'order_products/update'
-  end
-  namespace :admin do
-    get 'products/new'
-    get 'products/index'
-    get 'products/show'
-    get 'products/edit'
-  end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#top'
   get 'about' => 'homes#about'
 
