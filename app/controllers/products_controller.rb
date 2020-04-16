6ecf24d1ecd487e701b85c5f7cc5055b651efb52
vagrant <vagrant@localhost.localdomain>
2020-04-16 09:52:56 +0000

    genreadd

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,11 @@
 class ProductsController < ApplicationController
   def index
+  	@genres  = Genre.all
+  	if params[:i]
+  		@products = Product.where(genre_id: params[:i])
+  	else
+  		@products = Product.all
+  	end
   end
 
   def show
