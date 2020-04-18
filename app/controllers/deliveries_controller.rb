3d0bf306180751479bc3cae3d5fd919857785363
Shww5 <gcbas1313@gmail.com>
2020-04-18 14:11:32 +0000

    add delivery

---
+++ app/controllers/deliveries_controller.rb
@@ -0,0 +1,36 @@
+class DeliveriesController < ApplicationController
+before_action :authenticate_member!
+
+  def create
+  delivery = Delivery.new(delivery_params)
+  delivery.member_id = current_member.id
+  delivery.save
+  redirect_to deliveries_path
+  end
+
+  def index
+  @deliveries = Delivery.all
+  @delivery = Delivery.new
+  end
+
+  def edit
+  @delivery = Delivery.find(params[:id])
+  end
+
+  def update
+  delivery= Delivery.find(params[:id])
+  delivery.update(delivery_params)
+  redirect_to deliveries_path
+  end
+
+  def destroy
+  @delivery= Delivery.find(params[:id])
+  @delivery.destroy
+  redirect_to deliveries_path
+  end
+
+  private
+  def delivery_params
+   params.require(:delivery).permit(:postal_code, :address, :name)
+  end
+end
