9b6908fe0fc25e6460d2097c900f57e0322f1057
matsubishi5 <matsubishi5@yahoo.co.jp>
2020-04-19 02:00:50 +0000

    order2

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,18 +3,27 @@ class OrdersController < ApplicationController
     @order = Order.new
   end
 
-  def create
-  end
-
-  def verification
-    @cart_items = current_member.cart_items
-    @order_item = OrderItem.new
-  end
-
   def confirm
+    @order = Order.new(order_params)
+    @order_product = OrderProduct.new
+    @cart_items = current_member.cart_items
+  end
+
+  def create
+    order = Order.new(order_params)
+    order.member_id = current_member.id
+    order.postage = params[:postage].to_i
+    order.total_price = params[:total_price].to_i
+    order.order_status = params[:order_status].to_i
+    order.save
     redirect_to thanks_path
   end
 
+
   def thanks
+  end
+
+  def order_params
+    params.require(:order).permit(:member_id,:postal_code, :address, :name, :pay_type, :postage, :total_price, :order_status)
   end
 end
--- app/models/oder.rb
+++
@@ -1,8 +0,0 @@
-class Oder < ApplicationRecord
-  belongs_to :member
-
-  has_many :order_products, dependent: :destroy
-
-	enum order_status: [:waiting_payment, :payment_confirmation, :production, :preparing_shipping, :shipped]
-	#入金待ち0 #入金確認1 #製作中2 #発送準備中3 #発送済4
-end
