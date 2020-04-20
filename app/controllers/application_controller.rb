6ffda7baceac79bfc4fb981937857fee61bbf098
matsubishi5 <matsubishi5@yahoo.co.jp>
2020-04-20 07:02:16 +0000

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,23 @@
 class ApplicationController < ActionController::Base
 before_action :configure_permitted_parameters, if: :devise_controller?
+before_action :authenticate_member!, only: [:show,:edit]
 
-	def after_sign_in_path_for(resource)
-  	case resource
-  	when Admin
-    	admin_top_path
-  	end
-		end
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def after_sign_up_path_for(resource)
+    genres_path
+  end
 
   def after_sign_out_path_for(resource)
-    root_path
+    p resource
+    case resource
+    when :admin
+      new_admin_session_path
+    when :member
+      root_path
+    end
   end
 
   def configure_permitted_parameters
