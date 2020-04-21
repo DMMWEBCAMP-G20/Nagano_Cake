5c305c7d1998b1dab030cd8084b95efe28906f0c
yuusuke-t <confianca0525@gmail.com>
2020-04-21 08:58:02 +0000

    layouts

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,7 +1,7 @@
 class Admin::MembersController < ApplicationController
 	# before_action :authenticate_member!
   def index
-    @members = Member.page(params[:pege])
+    @members = Member.page(params[:page]).per(10)
   end
 
   def show
