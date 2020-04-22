58f376795ced9d31c0f7e144f0e0b28e1ed7b5d7
yuusuke-t <confianca0525@gmail.com>
2020-04-22 09:50:53 +0000

    layouts3

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,9 +3,10 @@ class ProductsController < ApplicationController
       @genres  = Genre.all
       p params[:i].class
       if params[:i]
-        @products = Product.where(genre_id: params[:i])
+        products = Product.where(genre_id: params[:i])
+        @products = products.page(params[:page]).per(5)
       else
-        @products = Product.all
+        @products = Product.page(params[:page]).per(5)
       end
   end
 
